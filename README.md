b39b70625ff5b9ac245158fa1444b90e2fbf97b6
Bora Ciner <bora.ciner@audivo.com>
2021-05-30 11:10:14 +0200

    mechanical switch added.

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Electron.js Application for Raspberry Pi
 
 1. Clone the repo
 ```sh
-git clone https://github.com/boraciner/electron-gpio
+git clone git@github.com:boraciner/electron-gpio.git
 ```
 2. Install NPM packages
 ```sh
--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path')
 var Gpio = require('onoff').Gpio;
 var led4 = new Gpio(4, 'out');
-
+var button18 = new Gpio(18, 'in');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
     webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
@@ -32,10 +32,13 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-
+button18.watch((err, value) => 
+  {
+    console.log("button : "+value)
+  }
+);
 
 ipcMain.on('GPIO', (event, arg) => {
-  
     console.log("GPIO",arg)
     if(arg==='ON'){
         led4.write(1)
